3908d99163acd36ad048b5bc89fe9b353464c29f
Cynthesizer <Cynthiaaay@gmail.com>
2018-10-03 12:22:53 -0400

    Add files via upload

--- splitSubjects_withTime.R
+++ splitSubjects_withTime.R
@@ -7,48 +7,52 @@ setwd("/Users/cynthiayue/Desktop/School/Moon_UX/output")
 #This first part to run thru all the files is taken from Test.R
 files <- list.files(pattern = "\\.(csv|CSV)$")  #just chews through all the csv files in a folder
 print(files)
+allSubj = list()
+subjCount=1
 
 # Go through all the files in the folder
-for (k in 1:length(files)){ #Currently this is only going through 1 file. Change 1 to length(files)
-    ID=(files[k]) 
-    print(ID) #lets you know its progress
-   
-    data <- read.csv(files[k], stringsAsFactors = FALSE, header=FALSE)  #Getting each data file to R:
-    dataTS <- data[,1] #Exract Time Stamp column
-    
-    # Initiating variables and vectors
-    timeSt <- vector()
-    count=1
-    
-    # Go thru each line in TimeStamp column to check for "timestamp"
-    for (i in 1:length(dataTS)) {
+for (k in 1:length(files)){ 
+  ID=(files[k]) 
+  print(ID) #lets you know its progress
   
-        if (dataTS[i] == " timestamp"){
-          timeSt[count]=i #nth bin of timeSt vector takes i, which is the line number for timeStamp title
-          count=count+1
-          cat("In file", ID, "line",i,"is the start of a new subject", "\n" ) #Print which line the new subject starts
-     
-        }
-    }
+  data <- read.csv(files[k], stringsAsFactors = FALSE, header=FALSE)  #Getting each data file to R:
+  dataTS <- data[,1] #Exract Time Stamp column
+  
+  # Initiating variables and vectors
+  timeSt <- vector()
+  count=1
+  
+  # Go thru each line in TimeStamp column to check for "timestamp"
+  for (i in 1:length(dataTS)) {
     
-    subj=1   #allSubj <- list()
-    for (m in 1:length(timeSt)) {
-        startLine = timeSt[m]+1
-        
-        if (timeSt[m]==timeSt[length(timeSt)]) { #Dealing with the end of the data
-          #endLine = timeSt[length(timeSt)]
-          endLine = length(dataTS)
-        } else {endLine = timeSt[m+1]-2}
-        
-        #Save subject data
-        dataSubj <- data[startLine:endLine,] #extract data for the subject
-        name <- paste(m,ID, sep="")
-        assign(name,dataSubj)
-        subj=subj+1
-        allSubj[subjCount]<-dataSubj
-        subjCount = subjCount + 1
-    }  
+    if (dataTS[i] == " timestamp"){
+      timeSt[count]=i #nth bin of timeSt vector takes i, which is the line number for timeStamp title
+      count=count+1
+      cat("In file", ID, "line",i,"is the start of a new subject", "\n" ) #Print which line the new subject starts
+      
+    }
+  }
+  
+  subj=1   #allSubj <- list()
+  for (m in 1:length(timeSt)) {
+    startLine = timeSt[m]
+    
+    if (timeSt[m]==timeSt[length(timeSt)]) { #Dealing with the end of the data
+      #endLine = timeSt[length(timeSt)]
+      endLine = length(dataTS)
+    } else {endLine = timeSt[m+1]-2}
+    
+    #Save subject data
+    dataSubj <- data[startLine:endLine,] #extract data for the subject
+    name <- paste(m,ID, sep="")
+    assign(name,dataSubj)
+    subj=subj+1
+    allSubj[subjCount]<-dataSubj
+    subjCount = subjCount + 1
+    
+  }  
 }
+
 
 
 ##trying to get time difference, testing with one dataset
